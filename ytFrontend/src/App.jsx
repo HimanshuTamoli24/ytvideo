10aa72599c27ce018cee35da505a5b1f7c97567f
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-02-23 00:07:35 +0530

    test done okay

--- ytFrontend/src/App.jsx
+++ ytFrontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (<>
-    <h1 className='text-7xl'>Welcome to YTVideo</h1>
+    <h1 className=' bg-black text-7xl'>Welcome to YTVideo</h1>
   </>
   )
 }
